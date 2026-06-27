c246e47ef3d9e9b68a2131d08b07a25e7d852834
Aastha gupta <aiml.aastha.2006@gmail.com>
2026-06-28 00:01:39 +0530

    final updation

--- src/main/java/com/quizwiz/servlet/SubmitServlet.java
+++ src/main/java/com/quizwiz/servlet/SubmitServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarata.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Timestamp;
